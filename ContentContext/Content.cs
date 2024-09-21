66b4858dd22ca78ac8b65a252a3c930330ff8b93
Lucas Samel <lucas.samel97@gmail.com>
2024-09-21 14:53:49 -0300

    Creating classes and alter project name

---
+++ ContentContext/Content.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payments.ContentContext
+{
+    public abstract class Content
+    {
+        public Content()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+}
