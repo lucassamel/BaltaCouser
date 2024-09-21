27ced69cc8912791ca75f8841df748c1e65a34a3
Lucas Samel <lucas.samel97@gmail.com>
2024-09-21 15:46:29 -0300

    Using base ctor

--- ContentContext/Course.cs
+++ ContentContext/Course.cs
@@ -2,7 +2,7 @@
 {
     public class Course : Content
     {
-        public Course()
+        public Course(string title, string url):base(title, url)
         {
             Modules = [];
         }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BaltaCourse.ContentContext;
+
+namespace BaltaCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var articles = new List<Article>();
+            articles.Add(new Article("Artigo sobre OOP", "orientacao-objetos"));
+            articles.Add(new Article("Artigo sobre C#", "orientacao-objetos"));
+            articles.Add(new Article("Artigo sobre .Net", "orientacao-objetos"));
+
+            foreach (var article in articles)
+            {
+                Console.WriteLine(article.Title);
+            }
+        }
+    }
+}
